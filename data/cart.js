7145a511b514b9e6cc070c283e626119ac992939
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-07-31 20:48:21 +0530

    adding products to cart for more enchaning

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,15 @@
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 1,
+  },
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }
+];
 
-export const cart = [];
-
-export function addToCart(productId){
+export function addToCart(productId) {
   let machingItem;
 
   cart.forEach((cartItem) => {
